00fafc114a6d6c7a2ceb447bfac80002aa60c6b8
rhysd <lin90162@yahoo.co.jp>
2019-01-31 16:24:50 +0900

    add tests: private enum and avoid dead code warning

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,11 +1,14 @@
 #![cfg(test)]
 
+// Separated from src/lib.rs since proc macro cannot be used at the same place
+// where it is defined.
+
 extern crate diff_enum;
 
 use diff_enum::common_fields;
 
 #[test]
-fn basic() {
+fn public_enum() {
     #[common_fields {
         x: i32,
     }]
@@ -15,6 +18,23 @@ fn basic() {
     }
 
     let e = E::A { b: true, x: 42 };
+    assert_eq!(e.x(), &42);
+
+    let e = E::B { x: 12 };
+    assert_eq!(e.x(), &12);
+}
+
+#[test]
+fn private_enum() {
+    #[common_fields {
+        x: i32,
+    }]
+    enum E {
+        A,
+        B,
+    }
+
+    let e = E::A { x: 42 };
     assert_eq!(e.x(), &42);
 
     let e = E::B { x: 12 };
@@ -93,4 +113,19 @@ fn derive_enum() {
 
     let s = format!("{:?}", E::B { x: 12 });
     assert_eq!(&s, "B { x: 12 }");
+}
+
+#[test]
+fn avoid_accessor_dead_code_warning() {
+    #[common_fields {
+        x: i32,
+    }]
+    #[deny(dead_code)]
+    enum E {
+        A,
+        B,
+    };
+
+    // Use E
+    E::A { x: 42 };
 }
